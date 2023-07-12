1a50aa552a6ca052da7ebd12edd8c8edf6c311e7
Faruk Erdem <farukomererdem2003@gmail.com>
2023-07-12 10:05:26 +0300

    Broadcasting

--- client.py
+++ client.py
@@ -1,5 +1,16 @@
 import socket
 import argparse
+import threading
+
+
+def receive_messages():
+    while True:
+        message = client.recv(1024).decode()
+        print(f"{message}\n")
+        if message == "exit":
+            break
+
+    client.close()
 
 
 def send_message():
@@ -11,16 +22,23 @@ def send_message():
 
     client.close()
 
+
 def get_user_input():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d","--destination",help="Destination server IP address.",dest="dest")
-    parser.add_argument("-p","--port",help="Server port number.",dest="port")
+    parser.add_argument("-d", "--destination",
+                        help="Destination server IP address.", dest="dest")
+    parser.add_argument(
+        "-p", "--port", help="Server port number.", dest="port")
     args = parser.parse_args()
     return args
+
 
 args = get_user_input()
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((args.dest, args.port))
+client.connect((str(args.dest), int(args.port)))
+
+receive_thread = threading.Thread(target=receive_messages)
+receive_thread.start()
 
 send_message()
--- server.py
+++ server.py
@@ -2,24 +2,43 @@ import threading
 import socket
 import json
 
+clients = []
+
+
+def broadcast_message(clients, message):
+    for client in clients:
+        client.send(message.encode())
+
+
+def add_new_client(client):
+    if not (client in clients):
+        clients.append(client)
+
+
 def handle_client(client):
     while True:
         message = client.recv(1024).decode()
-        print("Received message:", message)
-        if message == "exit":
-            break
+        print(f"{message}\n")
+        broadcast_message(clients, message)
 
-    client.close()
 
-def start_server(ip,port):
-    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server.bind(ip,port)
-    server.listen(10)
-    print(f"Server started, listening on port {port}")
+def start_server(ip, port):
+    try:
 
-    while True:
-        client, client_address = server.accept()
-        print(f"Accepted connection from: {client_address}")
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((ip, port))
+        server.listen(10)
+        print(f"Server started, listening on port {port}")
 
-        client_thread = threading.Thread(target=handle_client,args=(client,))
-        client_thread.start()
+        while True:
+            client, client_address = server.accept()
+            print(f"Accepted connection from: {client_address}\n")
+            add_new_client(client)
+            client_thread = threading.Thread(
+                target=handle_client, args=(client,))
+            client_thread.start()
+    except KeyboardInterrupt:
+        server.close()
+
+
+start_server("localhost", 8000)
